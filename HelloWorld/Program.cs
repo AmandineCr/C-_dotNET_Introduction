e5339a5ac46d8ac1bb00e03a2c9161f9f6b84c92
Amandine CESAR <amandine.cesar@campus-numerique-in-the-alps.com>
2018-08-20 16:49:08 +0200

    Greatings with datetime

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime localDate = DateTime.Now;
+            Boolean morning = localDate.Hour >= 9 && localDate.Hour < 13;
+            Boolean afternoon = localDate.Hour >= 13 && localDate.Hour <= 18;
+
+
+            DayOfWeek today = localDate.DayOfWeek;
+            Collection<DayOfWeek> daysOfWeekend = new Collection<DayOfWeek>
+            {
+                DayOfWeek.Monday,
+                DayOfWeek.Saturday,
+                DayOfWeek.Sunday
+            };
+
+            if (daysOfWeekend.Contains(today))
+                Console.WriteLine("Bon weekend !");
+            else
+                if (morning)
+                Console.WriteLine("Bonjour c'est le matin");
+            else if (afternoon)
+                Console.WriteLine("Bon après-midi");
+            else
+                Console.WriteLine("Bonsoir");
+
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
